c249c5925d574b8d4b71f559cc2461d4afb4410e
Andreas Knab <knabar@gmail.com>
2013-01-25 10:15:17 +0100

    Add video upload code with hardcoded metadata

--- upload.py
+++ upload.py
@@ -15,3 +15,32 @@ yt_service.password = getpass.getpass(prompt='Password for %s: ' % YT_USERNAME)
 yt_service.source = 'youtube-upload-test'
 yt_service.ProgrammaticLogin()
 
+
+title = 'MRI Stack'
+description = 'Volume rendering of ImageJ\'s "MRI Stack" sample dataset.'
+keywords = 'sample, mri'
+
+# prepare a media group object to hold our video's meta-data
+my_media_group = gdata.media.Group(
+    title=gdata.media.Title(text=title),
+    description=gdata.media.Description(description_type='plain',
+                                        text=description),
+    keywords=gdata.media.Keywords(text=keywords),
+    category=[gdata.media.Category(
+      text='Tech',
+      scheme='http://gdata.youtube.com/schemas/2007/categories.cat',
+      label='Science & Technology')],
+    player=None
+)
+
+# create the gdata.youtube.YouTubeVideoEntry to be uploaded
+video_entry = gdata.youtube.YouTubeVideoEntry(media=my_media_group)
+
+# set the path for the video file binary
+video_file_location = 'samples/mri-stack.mov'
+
+print "Uploading"
+
+new_entry = yt_service.InsertVideoEntry(video_entry, video_file_location)
+
+print "Done"
